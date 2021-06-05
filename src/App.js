eb5e87942ae196dde68af92f1829bd71e18ec303
aqiel14 <72314836+aqiel14@users.noreply.github.com>
2021-06-06 04:33:22 +0700

    initial commit

---
+++ src/App.js
@@ -0,0 +1,54 @@
+import React, { Component } from 'react';
+import Header from './components/header';
+import Sidebar from './components/sidebar';
+import Footer from './components/footer';
+import Dashboard from './components/dashboard';
+import Login from './components/login';
+import Register from './components/register';
+import Profile from './components/profile';
+import {
+  BrowserRouter as Router,
+  Route,
+  Redirect,
+  Switch,
+} from 'react-router-dom';
+
+const isLoggedIn = () => {
+  return localStorage.getItem('TOKEN_KEY') != null;
+};
+
+const SecuredRoute = ({ component: Component, ...rest }) => (
+  <Route
+    {...rest}
+    render={(props) =>
+      isLoggedIn() === true ? (
+        <Component {...props} />
+      ) : (
+        <Redirect to='/login' />
+      )
+    }
+  />
+);
+
+export default class App extends Component {
+  render() {
+    return (
+      <Router>
+        <Switch>
+          <div>
+            {isLoggedIn() && (
+              <>
+                <Header /> <Sidebar />
+              </>
+            )}
+            <Route path='/register' component={Register} />
+            <Route path='/login' component={Login} />
+            <Route path='/profile' component={Profile} />
+            <SecuredRoute path='/dashboard' component={Dashboard} />
+            {isLoggedIn() && <Footer />}
+          </div>
+        </Switch>
+      </Router>
+    );
+  }
+}
